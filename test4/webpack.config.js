880b587c05964f503cecf30630fa68f0e33a6679
zhangtanlin90@126.com <zhangtanlin90@126.com>
2017-06-07 09:43:27 +0800

    利用node的path模块填写exclude和include的绝对路径的方法

--- test4/webpack.config.js
+++ test4/webpack.config.js
@@ -1,4 +1,6 @@
 var htmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
+
 module.exports = {
   entry: {
     "main": "./src/app.js"
@@ -17,18 +19,21 @@ module.exports = {
       /**
       处理js文件
       test：匹配js文件
-      exclude:表示不处理哪些文件【必须是绝对路径】
-      include:表示需要处理的范围【必须是绝对路径】
+      exclude:表示不处理哪些文件【必须是绝对路径】/atom/muke_webpack/test4/node_modules/
+      include:表示需要处理的范围【必须是绝对路径】/atom/muke_webpack/test4/src/
       loader：处理的方法【babel-loader方法参考官方文档,需要安装babel-preset-latest依赖】
       注意1：babel-loader需要再根目录配置.balelrc文件；或者在package里添加
         "babel":{"presets":["latest"]}；再或者在loader: "babel-loader"后面添加
         query:{"presets":["latest"]}
+        【利用banel-loader的presets指定插件可以转换js语法】
       注意2：babel-loader的转换是非常耗时的，所以处理起来非常慢【所以需要改善】。
+      注意3：exclude和include的值还可以使用node的path模块里面resolve方法解析
+        其中__dirname表示当前目录。
       */
       {
         test: /\.js$/,
-        exclude: "/atom/muke_webpack/test4/node_modules/",
-        include: "/atom/muke_webpack/test4/src/",
+        exclude: path.resolve(__dirname, "node_modules"),
+        include: path.resolve(__dirname, "src"),
         loader: "babel-loader"
       }
     ]
